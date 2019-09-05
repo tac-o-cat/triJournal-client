e2a56c36bc068ee529fb02b8759c1dc616762a09
mini150146 <mini150146@gmail.com>
2019-09-05 18:48:01 +0900

    :sparkles: Render diaries and implement pagination

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Diary from "./Diary";
+import LoadingDiary from "./LoadingDiary";
+import { Pagination } from "antd";
 
 const List = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [numOfItems, setNumOfItems] = useState({ minValue: 0, maxValue: 10 });
+  const { minValue, maxValue } = numOfItems;
+  const [diaries, setDiaries] = useState([]);
+  const fetchDiary = async () => {
+    const diaries = await fetch("https://koreanjson.com/todos").then(res =>
+      res.json()
+    );
+    setDiaries(diaries);
+  };
+  useEffect(() => {
+    fetchDiary();
+    setLoading(false);
+  }, []);
+  const handleChange = page => {
+    if (page <= 1) {
+      setNumOfItems({
+        minValue: 0,
+        maxValue: 9
+      });
+    }
+    if (page > 1) {
+      setNumOfItems({ minValue: maxValue, maxValue: page * 10 });
+    }
+  };
+  return (
+    <div>
+      {loading ? (
+        <LoadingDiary />
+      ) : (
+        diaries
+          .slice(minValue, maxValue)
+          .map(diary => <Diary diary={diary} key={diary.id} />)
+      )}
+      <Pagination
+        size="small"
+        hideOnSinglePage={true}
+        pageSize={5}
+        onChange={handleChange}
+        total={diaries.length}
+      ></Pagination>
+    </div>
+  );
 };
 
 export default List;
