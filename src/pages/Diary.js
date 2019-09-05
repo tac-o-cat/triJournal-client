1e6d4ac6062e8ce118369377660c3b18d651f4b5
mini150146 <mini150146@gmail.com>
2019-09-05 18:47:06 +0900

    :sparkles: Create card to show diary content

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,7 +1,43 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { Card, Icon } from "antd";
 
-const Diary = () => {
-  return <div></div>;
+const Diary = props => {
+  const { Meta } = Card;
+  const date = props.diary.createdAt.slice(0, 10);
+  return (
+    <Card
+      style={{ width: 400 }}
+      loading={props.loading}
+      cover={
+        props.diary.image ? (
+          <img alt={props.diary.createdAt} src={props.diray.image} />
+        ) : (
+          undefined
+        )
+      }
+      actions={[
+        <Icon type="edit" key="edit" />,
+        <Icon type="delete" key="delete" />,
+        <Icon type="file-text" key="file-text" />
+      ]}
+    >
+      {props.loading ? (
+        <Meta />
+      ) : (
+        <Meta
+          title={date}
+          description={
+            <div>
+              <p>1. {props.diary.title}</p>
+              <p>2. {props.diary.title}</p>
+              <p>3. {props.diary.title}</p>
+            </div>
+          }
+        />
+      )}
+    </Card>
+  );
 };
 
 export default Diary;
