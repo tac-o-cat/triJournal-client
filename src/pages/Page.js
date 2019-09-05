4d84e538212dab24a7e37d7d215c28a8d9f91b50
mini150146 <mini150146@gmail.com>
2019-09-05 18:59:39 +0900

    :lipstick: Change header color

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout, Icon } from "antd";
+import { Layout } from "antd";
 import Write from "./Write";
 import NavBar from "./NavBar";
 import List from "./List";
@@ -9,9 +9,7 @@ const Page = () => {
   const { Header, Content } = Layout;
   return (
     <Router>
-      <Header style={{ backgroundColor: "yellow" }}>
-        {/*나중에 색깔 바꾸기*/}
-        <Icon type="menu-fold" />
+      <Header style={{ backgroundColor: "#d3adf7" }}>
         <Route path="/page" component={NavBar} />
         <span>triJournal</span>
       </Header>
