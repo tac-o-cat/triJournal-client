06a06745d136820dce6fcabdc876e5eb8f1a5d95
mini150146 <mini150146@gmail.com>
2019-09-06 17:44:43 +0900

    :zap: Connect FindPw component to server

--- src/pages/FindPw.js
+++ src/pages/FindPw.js
@@ -3,6 +3,7 @@ import { Input, Button } from "antd";
 import setDataToState from "../modules/setDataToState";
 
 const FindPw = () => {
+  const API_HOST_URL = process.env.REACT_APP_API_HOST_URL;
   const InputGroup = Input.Group;
   const [user, setUser] = useState({ emailId: "", address: "", id: "" });
   const { emailId, address, id } = user;
@@ -10,6 +11,24 @@ const FindPw = () => {
     setDataToState(e.target.id, setUser, user);
   };
   const handleClick = () => {
+    let email = { email: `${emailId}@${address}` };
+    fetch(`${API_HOST_URL}/users/findPassword`, {
+      method: "POST",
+      body: JSON.stringify(email),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        let message = document.getElementById("message");
+        if (res.password) {
+          message.innerHTML = `회원님의 비밀번호는 ${res.password}입니다.`;
+        } else {
+          message.innerHTML = "해당 이메일로 가입한 아이디가 없습니다.";
+        }
+      })
+      .catch(err => console.error(err));
     // 유저 아이디와 이메일을 body에 담아 서버에 포스트 요청을 보낸다.
     // pw를 바로 받는 방법? 서버에서 인증 이메일을 보내는 방법? 논의 필요함.
   };
@@ -42,6 +61,7 @@ const FindPw = () => {
         />
         <Input id="address" style={{ width: "55%" }} onChange={handleChange} />
       </InputGroup>
+      <div id="message"></div>
       <Button
         style={{ margin: "3px 3px 3px 3px" }}
         type="primary"
