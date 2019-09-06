df180acbeb5b2a2a4301f1435af9521d1d7d70fa
mini150146 <mini150146@gmail.com>
2019-09-06 16:57:27 +0900

    :zap: Connect FindId component to server

--- src/pages/FindId.js
+++ src/pages/FindId.js
@@ -3,17 +3,33 @@ import { Input, Button } from "antd";
 import setDataToState from "../modules/setDataToState";
 
 const FindId = () => {
+  const API_HOST_URL = process.env.REACT_APP_API_HOST_URL;
   const InputGroup = Input.Group;
-  const [email, setEmail] = useState({ id: "", address: "" });
-  const { id, address } = email;
+  const [email, setEmail] = useState({ emailId: "", address: "" });
+  const { emailId, address } = email;
   const handleChange = e => {
     setDataToState(e.target.id, setEmail, email);
   };
   const handleClick = () => {
+    let email = { email: `${emailId}@${address}` };
+    fetch(`${API_HOST_URL}/users/findId`, {
+      method: "POST",
+      body: JSON.stringify(email),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        let message = document.getElementById("message");
+        if (res.username) {
+          message.innerHTML = `회원님의 아이디는 ${res.username}입니다.`;
+        } else {
+          message.innerHTML = "해당 이메일로 가입한 아이디가 없습니다.";
+        }
+      });
     // email을 body에 담아 서버에 포스트 요청을 보낸다.
     // id가 리스폰스로 오면, 해당 응답을 message div에 출력한다.
-    let message = document.getElementById("message");
-    message.innerHTML = "해당 이메일로 가입한 아이디가 없습니다.";
   };
   return (
     <div
@@ -28,7 +44,7 @@ const FindId = () => {
       <div style={{ margin: "3px 3px 3px 3px" }}>이메일을 입력해 주세요.</div>
       <InputGroup compact style={{ margin: "3px 3px 3px 3px" }}>
         <Input
-          id="id"
+          id="emailId"
           style={{ width: "45%" }}
           addonAfter="@"
           onChange={handleChange}
@@ -39,7 +55,7 @@ const FindId = () => {
       <Button
         type="primary"
         onClick={handleClick}
-        disabled={id.length && address.length ? false : true}
+        disabled={emailId.length && address.length ? false : true}
         style={{ margin: "3px 3px 3px 3px" }}
       >
         완료
