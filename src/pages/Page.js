a539f1c8537c630076aa7c5b1ec37f7c352e22ab
mini150146 <mini150146@gmail.com>
2019-09-07 21:42:34 +0900

    :work_in_progress: Fix fetching written diaries

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,13 +1,25 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from "antd";
 import Write from "./Write";
 import NavBar from "./NavBar";
 import List from "./List";
 import MyInfo from "./MyInfo";
+const API_HOST_URL = process.env.REACT_APP_API_HOST_URL;
 
-const Page = () => {
+const Page = props => {
   const { Header, Content } = Layout;
+  const [currentUser, setCurrentUser] = useState({ username: undefined });
+  useEffect(() => {
+    let isSubscribed = true;
+
+    fetch(`${API_HOST_URL}/users/${props.location.state.username}`)
+      .then(res => res.json())
+      .then(res => (isSubscribed ? setCurrentUser(res) : null));
+    return () => (isSubscribed = false);
+  }, [props.location.state.username]);
+
   return (
     <Router>
       <Header style={{ backgroundColor: "#d3adf7" }}>
@@ -16,7 +28,10 @@ const Page = () => {
       </Header>
       <Content style={{ padding: "30px 50px 50px 50px" }}>
         <Switch>
-          <Route path="/page/write" component={Write} />
+          <Route
+            path="/page/write"
+            component={() => <Write username={currentUser.username} />}
+          />
           <Route path="/page/list" component={List} />
           <Route path="/page/myInfo" component={MyInfo} />
         </Switch>
--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import { Input, Button, Empty } from "antd";
 import setDataToState from "../modules/setDataToState";
 import Diary from "./Diary";
+const API_HOST_URL = process.env.REACT_APP_API_HOST_URL;
 
-const Write = () => {
+const Write = props => {
   const style = {
     margin: "3px 3px 3px 3px"
   };
@@ -17,30 +19,47 @@ const Write = () => {
     image: undefined
   });
 
-  const { best, worst, todo } = journal;
-
+  const { best, worst, todo, image } = journal;
   const fetchDiary = async () => {
-    const diaries = await fetch("https://koreanjson.com/todos").then(res =>
-      res.json()
-    );
-    setDiaries(diaries);
-    setLoading(false);
+    if (props.username) {
+      let writtenDaries = await fetch(`${API_HOST_URL}/posts/mini1`)
+        .then(res => res.json())
+        .then(res => console.log(res));
+      if (Array.isArray(writtenDaries)) setDiaries(writtenDaries);
+      setLoading(false);
+    }
   };
   useEffect(() => {
     fetchDiary();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleChange = e => {
     setDataToState(e.target.id, setJournal, journal);
   };
   const handleClick = () => {
     // 서버로 post 요청을 보낸다.
+    let diary = {
+      best: best,
+      worst: worst,
+      todo: todo,
+      longLog: undefined,
+      picUrl: image,
+      userName: props.username
+    };
+    fetch(`${API_HOST_URL}/posts/${props.username}`, {
+      method: "POST",
+      body: JSON.stringify(diary),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(res => res.json());
   };
+  // eslint-disable-next-line no-unused-vars
   const setImage = file => {
     setJournal({ ...journal, image: file });
   };
   return (
     <div style={{ width: "50%", margin: "0 50px 0 50px" }}>
-
       <InputGroup style={{ width: "70%" }}>
         <Input
           id="best"
@@ -68,6 +87,7 @@ const Write = () => {
       >
         작성
       </Button>
+      {console.log(loading)}
       {!diaries[0].id && !loading ? ( // 들어온 다이어리에 id가 배정되어 있지 않다면 초기에 스테이트로 설정된 빈 오브젝트이므로
         <Empty // Empty 컴포넌트 돌려줌.
           image={Empty.PRESENTED_IMAGE_SIMPLE}
