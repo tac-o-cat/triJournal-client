35867e0b8ff7a6fcb731c0376f90beb19382e83e
mini150146 <mini150146@gmail.com>
2019-09-08 15:20:08 +0900

    :sparkles:  Implement login and redirection feature

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import FindIdPw from "./pages/FindIdPw";
 import Page from "./pages/Page";
+import Main from "./pages/Main";
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Main} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
         <Route path="/page" component={Page} />
