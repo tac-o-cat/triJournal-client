683f4659545a4eda3e9f30c64e63ec6c4a2917de
mini150146 <mini150146@gmail.com>
2019-09-08 17:36:21 +0900

    :art: Implement clear diary input boxes after posting

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -8,7 +8,7 @@ const API_HOST_URL = process.env.REACT_APP_API_HOST_URL;
 
 const Write = props => {
   const style = {
-    margin: "3px 3px 3px 3px"
+    margin: "3px 3px 3px 3px"
   };
   const InputGroup = Input.Group;
   const [loading, setLoading] = useState(true);
@@ -19,26 +19,24 @@ const Write = props => {
     todo: "",
     image: undefined
   });
-
   const { best, worst, todo, image } = journal;
   const fetchDiary = async () => {
     if (props.username) {
-      let writtenDaries = await fetch(`${API_HOST_URL}/posts/mini1`).then(res =>
-        res.json()
-      );
+      let writtenDaries = await fetch(
+        `${API_HOST_URL}/posts/${props.username}`
+      ).then(res => res.json());
       if (Array.isArray(writtenDaries)) setDiaries(writtenDaries);
       setLoading(false);
     }
   };
   useEffect(() => {
-    fetchDiary();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchDiary(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleChange = e => {
     setDataToState(e.target.id, setJournal, journal);
   };
-  const handleClick = () => {
-    // 서버로 post 요청을 보낸다.
+  const handleClick = async () => {
+    //서버로 post 요청을 보낸다.
     let diary = {
       best: best,
       worst: worst,
@@ -47,37 +45,47 @@ const Write = props => {
       picUrl: image,
       userName: props.username
     };
-    fetch(`${API_HOST_URL}/posts/${props.username}`, {
+    await fetch(`${API_HOST_URL}/posts/${props.username}`, {
       method: "POST",
       body: JSON.stringify(diary),
       headers: {
         "Content-Type": "application/json"
       }
-    }).then(res => res.json());
+    });
+    let loadNewDiary = await fetch(`${API_HOST_URL}/posts/mini1`).then(res =>
+      res.json()
+    );
+    setDiaries([...diaries, loadNewDiary[loadNewDiary.length - 1]]);
+    let bestInput = document.getElementById("best");
+    let worstInput = document.getElementById("worst");
+    let todoInput = document.getElementById("todo");
+    bestInput.value = "";
+    worstInput.value = "";
+    todoInput.value = "";
   };
   const setImage = file => {
     setJournal({ ...journal, image: file });
   };
   return (
-    <div style={{ width: "50%", margin: "0 50px 0 50px" }}>
+    <div style={{ width: "50%", margin: "0 50px 0 50px" }}>
       <InputGroup style={{ width: "70%" }}>
         <Input
           id="best"
           onChange={handleChange}
           style={style}
-          placeholder="오늘 가장 좋았던 일"
+          placeholder="오늘 가장 좋았던 일"
         />
         <Input
           id="worst"
           onChange={handleChange}
           style={style}
-          placeholder="오늘 가장 나빴던 일"
-        />
+          placeholder="오늘 가장 나빴던 일"
+        />{" "}
         <Input
           id="todo"
           onChange={handleChange}
           style={style}
-          placeholder="내일 할 일"
+          placeholder="내일 할 일"
         />
       </InputGroup>
       <Button
@@ -88,15 +96,24 @@ const Write = props => {
         작성
       </Button>
       <UploadImage setImage={setImage} currentImage={image} />
-      {!diaries[0].id && !loading ? ( // 들어온 다이어리에 id가 배정되어 있지 않다면 초기에 스테이트로 설정된 빈 오브젝트이므로
-        <Empty // Empty 컴포넌트 돌려줌.
+      {!diaries[0].id && !loading ? ( // 들어온 다이어리에 id가 없다면 초기에 설정해준 빈 오브젝트이므로
+        <Empty // Empty를 랜더한다.
           image={Empty.PRESENTED_IMAGE_SIMPLE}
           description={<span>첫 번째 일기를 써보세요!</span>}
         />
       ) : (
-        diaries
-          .slice(0, 10)
-          .map((diary, i) => <Diary diary={diary} loading={loading} key={i} />)
+        diaries.slice(diaries.length - 10).map((diary, i, tenDiaries) => {
+          console.log(tenDiaries);
+          let diaryIndex = tenDiaries.length - 1;
+          console.log(diaryIndex);
+          return (
+            <Diary
+              diary={tenDiaries[diaryIndex - i]}
+              loading={loading}
+              key={i}
+            />
+          );
+        })
       )}
     </div>
   );
