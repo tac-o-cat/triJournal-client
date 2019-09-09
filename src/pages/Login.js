3ad3ca129bcab4df405e5297986c7434abb528e3
mini150146 <mini150146@gmail.com>
2019-09-09 11:47:24 +0900

    :lipstick: Update login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,31 +9,36 @@ class Login extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
-  handleClick() {
+  handleClick(e) {
     const form = this.props.form;
-    let username = form.getFieldValue("id");
-    let password = form.getFieldValue("password");
-    let body = {
-      username: username,
-      password: password
-    };
-    fetch(`${API_HOST_URL}/sign/signin`, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json"
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        let username = values.id;
+        let password = values.password;
+        let body = {
+          username: username,
+          password: password
+        };
+        fetch(`${API_HOST_URL}/sign/signin`, {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+          .then(res => res.json())
+          .then(res => {
+            res.isLogIn
+              ? this.props.history.push({
+                  pathname: "/page/write",
+                  state: { username: form.getFieldValue("id") }
+                })
+              : alert("아이디, 비밀번호를 확인하세요");
+          })
+          .catch(err => console.error(err));
       }
-    })
-      .then(res => res.json())
-      .then(res => {
-        res.isLogIn
-          ? this.props.history.push({
-              pathname: "/page/write",
-              state: { username: form.getFieldValue("id") }
-            })
-          : alert("아이디, 비밀번호를 확인하세요");
-      })
-      .catch(err => console.error(err));
+    });
   }
   render() {
     const form = this.props.form;
@@ -49,43 +54,60 @@ class Login extends React.Component {
           textAlign: "center"
         }}
       >
-        <h3>triJournal</h3>
-        <Form className="login-form">
-          {form.getFieldDecorator("id", {
-            rules: [{ required: true, message: "Please input your username!" }]
-          })(
-            <Input
-              id="id"
-              compact="true"
-              style={{ ...style, width: "50%" }}
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="ID"
-            />
-          )}
-          {form.getFieldDecorator("password", {
-            rules: [{ required: true, message: "Please input your password!" }]
-          })(
-            <Input
-              id="password"
-              compact="true"
-              style={{ ...style, width: "50%" }}
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="password"
-            />
-          )}
-          <Button
-            onClick={this.handleClick}
-            style={{ margin: "3px 3px 3px 3px" }}
-          >
-            Login
-          </Button>
+        <Form className="login-form" onSubmit={this.handleClick}>
+          <Form.Item>
+            <h3>triJournal</h3>
+          </Form.Item>
+          <Form.Item>
+            {form.getFieldDecorator("id", {
+              rules: [
+                { required: true, message: "Please input your username!" }
+              ]
+            })(
+              <Input
+                id="id"
+                compact="true"
+                style={{ ...style, width: "50%" }}
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="ID"
+              />
+            )}
+          </Form.Item>
+          <Form.Item hasFeedback>
+            {form.getFieldDecorator("password", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please input your password!"
+                },
+                {
+                  validator: this.validateToNextPassword
+                }
+              ]
+            })(
+              <Input.Password
+                compact="true"
+                style={{ ...style, width: "50%" }}
+                placeholder="Password"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button htmlType="submit" style={{ margin: "3px 3px 3px 3px" }}>
+              Login
+            </Button>
+          </Form.Item>
         </Form>
-        <Link to="/signup">
-          <div>Sign Up</div>
-        </Link>
-        <Link to="/find">
-          <div>Find ID/Password</div>
-        </Link>
+        <Form.Item>
+          <div>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+          <div>
+            <Link to="/find">Find ID/Password</Link>
+          </div>
+        </Form.Item>
       </div>
     );
   }
@@ -94,10 +116,3 @@ class Login extends React.Component {
 const WrappedLogin = Form.create({ name: "login" })(Login);
 
 export default WrappedLogin;
-
-//App.js 브라우저라우터 라우터 라우트 링크 데려오기
-//App.js에서 라우터를 만든다 => path=/login으로 설정
-//Component의 Attribute는 Login으로 설정
-//Login.js 페이지에서 Id - <input> , pw - < input> , login - <button>, join - <div>, find - <div>
-//API
-//Login 클릭핳 때 post 요청
